b44daff65d8650af73d5339acd2d12e2140345ac
stevenkore <42966699+stevenkore@users.noreply.github.com>
2019-12-05 12:26:37 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,9 +5,22 @@ import {Category} from "./entity/Category";
 // connection settings are in the "ormconfig.json" file
 createConnection().then(async connection => {
 
+// UUS POST
+const post = new Post();
+post.title = "dsadasd";
+post.text = "dsadasd";
+
 const postRepo = connection.getRepository(Post);
 
+
+// DELETE
 await postRepo.delete({id : 33});
+
+
+// UPDATE
+await postRepo.update(21, {title: "adassd"});
+
+// SAVE
 connection.getRepository(Post).save(post);
 
 }).catch(error => console.log("Error: ", error));
